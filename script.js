6e3f31479fde6ce70e8592f73cabe0e12318561a
Gupta-deepak1906 <147096327+Gupta-deepak1906@users.noreply.github.com>
2024-09-25 23:32:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
     //Write your code here
+	const form =document.getElementByID("form1");
+
+	const formData = new FormData(form);
+	const firstName = formData.get('fname');
+	const lastName = formData.get('lname');
+
+	alert(firstName + " " + lastName)
 
 }
