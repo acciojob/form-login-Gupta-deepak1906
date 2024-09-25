f247e58e9b3ce4cea8a5c17c60cf461be1f523d3
Gupta-deepak1906 <147096327+Gupta-deepak1906@users.noreply.github.com>
2024-09-25 23:39:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 function getFormvalue() {
     //Write your code here
-	const form =document.getElementByID("form1");
+	const form =document.getElementById("form1");
 
 	const formData = new FormData(form);
 	const firstName = formData.get('fname');
 	const lastName = formData.get('lname');
+
+	//alternate way
+
+	// const firstName = form.elements['fname'].value;
+	//const lastName = form.elements['lname'].value;
+	
 
 	alert(firstName + " " + lastName)
 
